1b0a79bac85db8de460425c08e2abcf5dba47990
Kyle Barnes <ktbarnes@princeton.edu>
2020-08-17 14:03:08 +1000

    add scrollable activism work

---
+++ js/scroll.js
@@ -0,0 +1,234 @@
+// ADS :: phrases
+const adPhrases =
+      ["Get access to 30+ iconic tennis pros giving the secrets of their craft",
+       "Who's hungry y'all? Snoop remixed our jingle and cut us a new track.",
+      "Hurry - only a limited number of watches available!",
+      "A world first recycled reverse weave range created with 45% recycled materials. Get your fix now.",
+      "We've got cakes in a range of sizes and flavours to fancy all tastebuds",
+      "Try our cabbage+chicken fried rice and pork lard+egg friend rice, trust me, you'll like it!",
+      "Tap to shop our bestselling sweatshirt now!",
+      "DM us to organize your free consultation",
+      "Effortless precision for your style",
+      "Experience your skin's peak performance with Hydra Energetic Moisturiser",
+      "Our Food Court is open Mon-Fri from 6am for Dine-In or Take-Away's",
+      "You can trust us completely when it comes to screen repair or replacement",
+      "Try our Wellgrove Immune Support Olive Leaf Extract packed with antioxidants for peak self-care",
+      "Get FREE SHIPPING on all US orders when you shop now!",
+      "We're excited to re-open our 48 stores across the country",
+      "Strike, slay, and stun the competition using the top tier of Windows 10 Pro gaming",
+      "Buy one get one 35% OFF",
+      "SALE : 50% off TODAY ONLY",
+      "You asked -- we answered. Check out the newest items in stock",
+      "Get the silky smooth skin you want with Laser Hair Removal at Ideal Image",
+      "Watch #SharkWeek all week long starting at 8pm ET on Discovery.",
+      "Watch Bloodshot, plus thousands of hit movies on STARZ",
+      "Students save $100 on MacBook Air. We have all the Apple products you want",
+      "Plan your next camping trip in minutes with the #1 Camping App of 2020. Download today.",
+      "Discover amazing new wines with 50% off your first order",
+      "Win a VIP trip to The Governor's Ball Music Festival 2021",
+      "Try our Miracle Moisture Boost treatment to quench and nourish your dehydrated strands",
+      "Keep your summer style fresh with Gillette SkinGuard.",
+      "Spice up your kitchen with this D.I.Y. storage rack hack",
+      "Use Code: NEW2020 To Save 30-70% Off SITEWIDE & Get BOGO FREE On Select Sale Styles",
+      "The Perfect Look For Browsing The Shops At Your Fave Beach Town",
+      "When it comes to Strawberries, we’re as real as it gets. Tag your friends who keep it real!",
+      "The spotlight should be on you—NOT your acne",
+      "We’re already almost sold out on these new gold bracelets!!"];
+// ACTIVIST :: phrases
+const actPhrases =
+      ["Nobody should have to be ashamed of who they are. Let us enjoy our food in peace.",
+      "Whatever you do, dont just 'like' all these reading lists and book posts",
+      "We must keep educating ourselves for the rest of our lives",
+      "We need to do more within our school systems",
+      "We believe in a world where there are zero police murders because there are zero police",
+      "If you are a Black man who only shows up for Black men, you are the problem",
+      "Erasure is not equality",
+      "You cannot be transphobic and claim that Black Lives Matter",
+      "Being an Ally is not a part time job",
+      "This pride month cant just be a celebration, it has to be a riot",
+      "Why you should care about Yemen",
+      "If you're not using your afforded privilege to inform and uplift, you are not welcome around me.",
+      "Property can be replaced, human lives CANNOT",
+      "Things to do instead of calling the cops",
+      "Why are you performing gendered habits while staying home and social distancing?",
+      "Justice calls for a copless future",
+      "Burn it down.",
+      "Stop taking up space that isn't yours",
+      "Stop co-oping black anger, grief, and demands",
+      "Let's target our posts towards the people that need to see and hear it",
+      "There is no excuse for your silence",
+      "Black people are not required to educate you",
+      "The first step to getting free is admitting you have a colonized mind",
+      "Everything you come to understand about the human experience was taught to you from a white supremacist, capitalist, patriarichal frame of thinking.",
+      "Ways to donate to the White Mountain Apache Tribe",
+      "Where to send your donations for the Navajo",
+      "Where to donate in Minneapolis",
+      "Make Juneteenth a national holiday",
+      "Why we should celebrate Juneteenth",
+      "BUY, SHARE, FOLLOW, LIKE, STREAM, READ, SIGN, WATCH AND REPOST",
+      "This is a movement not a moment",
+      "We must stay outraged and show up even if it is imperfect",
+      "We must continue to amplify black voices daily and form habits that hold ourselves and others accountable",
+      "Tolerating racism is racism",
+      "The only dangerous minority is the rich",
+      "Vanessa is unable to user her voice. But you can use yours",
+      "Justice for Toyin",
+      "Justice for George Floyd",
+      "ICE just kicked thousands of students out of the United States",
+      "You are personally responsible for becoming more ethical than the society you grew up in",
+      "Justice for Breonna Taylor",
+      "Arrest the cops who killed Breonna Taylor",
+      "Anti-racism work is not self-improvement work for white people",
+      "Do you like death berries? Boycott Discoll's and buy local",
+      "Black youth are incarcerated over 4x as often as white youth",
+      "Grace is currently being held in a juvenile detention center for not completing her homework",
+      "Billionaires shouldn't exist",
+      "What's happening in McAllen, TX (and how you can help)",
+      "Please signal boost this.",
+      "All colonizers must fall",
+      "No borders on stolen land",
+      "It's fucking weird if your brand/business/work place has no BIPOC in management positions",
+      "As long as you're learning, you're not failing",
+      "Violence against property is not the same as violence against people",
+      "Black, Indigenous and other POC should not have to fund our liberation. We are not responsible for our oppression.",
+      "What to do if BLM has disappeared from your IG feed",
+      "Stay activated. Stay educated. Stay connected. We need you.",
+      "End racial bias in school discipline",
+      "The number of trans people murdered in 2020 just surpassed the total for last year. It's only been seven months",
+      "If you won't accept a queer child don't have kids",
+      "It's a great day to arrest the cops that killed Breonna Taylor",
+      "Arrest Myles Cosgrove, Brett Hankison and Jonathan Mattingly",
+      "Request your vote-by-mail ballot before your deadline",
+      "The United States Postal Service is on the brink of financial collapse",
+      "You cannot be pro-Israel and say that Black Lives Matter",
+      "We must listen. We must act.",
+      "Contact your elected representatives and urge them to support a plan to stop the climate crisis.",
+      "How do we decolonize while still living within colonial ways?",
+      "Land Back: A Guide to Returning Stolen Land To Your Indigenous Community",
+      "Mass incarceration is a public health crisis for everyone",
+      "Take a step back and check if your attraction for an individual perpetuates some form of harm towards a racial group.",
+      "Educate yourself",
+      "Vote, if needed, & go learn, imagine, build in community",
+      "I do not worship at the altar of the violent American Project",
+      "4 community action items you should complete TODAY",
+      "Ten petitions you should sign TODAY",
+      "Three Black owned businesses in Dallas you should support",
+      "It has been decided that India is a Hindu only state.",
+      "Why you should care about Armenia and Azerbaijan",
+      "Israel is an apartheid state",
+      "We carry and pass on the traumas planted in us by the colonisers",
+      "Blak Lives Matter",
+      "Black Lives Matter",
+      "Black Trans Lives Matter",
+      "Islamophobia is a disease",
+      "It's always 'listen to black people' until black people reject your 'lesser of two evils' rhetoric and the placid allyship that comes with it",
+      "The state only gives us the illusion of choice. The illusion of choice is fascism",
+      "Don't call COVID-19 the 'Chinese Virus' or 'Kung Flu'",
+      "Don't ask me where I'm 'really' from",
+      "Why you should care about Belarus","No war in Yemen","Black life is traumatizing. Black death is traumatizing","Justice for Ahmaud Arbery","10 simple ways white people can step up to fight everyday racism","Take action now","Poverty should not be a crime","We have been conditioned into complacency, performance, and meaningless guilt","Why you shouldn't have posted a black square","Advocacy means going beyond having conversations with people who already agree with you","87 ways you can help", "3 texts to send right now", "A list of Black queer and trans organizations to support"];
+
+// create div childs for the floor
+for(x=0; x<100;x++) {
+    var floor = document.createElement('div');
+    floor.className = "floor-square";
+    document.getElementById('floor').appendChild(floor);
+}
+
+// card colors
+const cardColors = ["#D9C9E6", "#14836C", "#FEC0C1", "#000000", "#F79577", "#232323", "#F5C165", "#232323", "#F9F9F9", "#FD0400","#FFFFFF", "#151F2B", "#FEDCED", "#C8D0E7", "#7A7F14", "#7108D4", "#FA503E", "#FED525", "#F24509", "#108AD7", "#F4EF22", "#1EAAFF", "#B6C2F6", "#46D9BF", "#27D560", "#FF1F86", "#D297EC", "#FFD000", "#2044E0", "#F13B29", "#ECC3BF", "#08A23E", "#FAFF33", "#4963D7", "#E47836", "#AC7661", "#AAC5D8", "#FAE6DF", "#6A315C", "#BAECED", "#B3D0D9", "#3E3A41", "#B16D5A", "#3A296D", "#839669", "#F7F2F1", "#E5D6CF", "#CDBDB0", "#E65F27", "#EAE3C6", "#EFDABB", "#3C93C3", "#869E88", "#957159", "#8BD6AF", "#FFDE59", "#D2E4EE", "#FFDDEA", "#FE4940", "#F21783", "#91ABDF", "#674041", "#FE1DD0", "#FEA735", "#B9ACB3", "#E0EDE4", "#5B9890", "#F3F2F0", "#F7F4ED", "#FBAA03", "#FBDDE2"]
+
+// make the colors random
+function randomCardColors() {
+  for (let i = 0 ; i < cardColors.length ; i++) {
+    return cardColors[Math.round(Math.random()*cardColors.length)]
+  }
+}
+
+// design the floor squares, and loop the random background color on them
+const floorSquare = document.getElementsByClassName("floor-square")
+
+for (var i = 0; i < 10000; i++) {
+  (function (i) {
+    var timeout = 3000*i;
+    if (i < 45) {
+      timeout = (3000 - 50*i)*i;
+    } else {
+      timeout = 800*i;
+    }
+    setTimeout(function() {
+      for (var x = 0; x < floorSquare.length; x++) {
+        floorSquare[x].style.backgroundColor = randomCardColors();
+      }
+    }, timeout);
+    })(i);
+};
+function randomPhrase() {
+  return actPhrases[Math.round(Math.random()*(actPhrases.length - 1))];
+}
+var listElm = document.querySelector('#infinite-list');
+
+// Add 24 items at a time
+var nextItem = 0;
+var prevPhrase = "";
+var loadMore = function() {
+  for (var i = 0; i < 24; i++) {
+    var item = document.createElement('li');
+    var newPhrase = randomPhrase();
+    if (newPhrase === prevPhrase) {
+      newPhrase = randomPhrase();
+    }
+    item.innerText = newPhrase;
+    if (i % 4 === 0) {
+      item.style.padding = "350px 10px 0px 100px";
+      item.style.textAlign = "right";
+      item.style.transform = "skewY(" + 0.75*nextItem + "deg)";
+      if (nextItem > 20) {
+        item.style.transform = "skewY(15deg)";
+      }
+    } else if (i % 4 === 2) {
+      item.style.padding = "350px 100px 0px 10px";
+      item.style.textAlign = "left";
+      item.style.transform = "skewY(-" + 0.75*nextItem + "deg)";
+      if (nextItem > 20) {
+        item.style.transform = "skewY(-15deg)";
+      }
+    } else if (i % 4 === 3) {
+      item.innerText = adPhrases[Math.round(Math.random()*(adPhrases.length - 1))];
+    }
+    var scaleAmt = 1 + 0.03*nextItem;
+    item.style.transform = "scaleY(" + scaleAmt +")";
+    listElm.appendChild(item);
+    nextItem++;
+  }
+}
+
+// Detect when scrolled to bottom.
+listElm.addEventListener('scroll', function() {
+  if (listElm.scrollTop + listElm.clientHeight >= listElm.scrollHeight) {
+    loadMore();
+  }
+});
+
+// Initially load some items.
+loadMore();
+
+// for cursor
+document.onmousemove = function(e) {
+  document.body.style.setProperty('--x',(e.clientX)+'px');
+  document.body.style.setProperty('--y',(e.clientY)+'px');
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// When the user clicks on modal, close it
+modal.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
