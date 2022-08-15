19124933a2a2fce2c4ea7d699264b5da71808e6e
k-y-le <41943646+k-y-le@users.noreply.github.com>
2022-08-15 13:58:57 -0400

    final changes !

--- home.js
+++ home.js
@@ -1,11 +1,17 @@
-$(".home-click").hover(function(){
+
+$(".home-click").hover(function(e){
+    e.stopPropagation();
     $("#home-text").attr("src", "img/text_" + this.id + ".png"); 
 }, function(){
     $("#home-text").attr("src", "img/text_kyle.png"); 
 })
 
-$(".home-click").click(function(){
+$(".home-click").click(function(e){
+    e.stopPropagation();
     $("#home-text").attr("src", "img/text_" + this.id + ".png");
     $(".info-box").hide();
     $(".more-info-" + this.id).show();
+})
+$("body").click(function(){
+    $('.info-box').hide();
 })
